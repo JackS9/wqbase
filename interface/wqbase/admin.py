95880af4c50f27e47d394f3eb8f6d927d4acfa0d
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2021-02-27 13:42:17 +0000

    Fix reimported error

--- interface/wqbase/admin.py
+++ interface/wqbase/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from django.contrib import admin
 from .models import Stations, Results
 admin.site.register(Stations)
 admin.site.register(Results)
