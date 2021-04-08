f18b728948cc94fe62f3410ce97fc89a620ed8c6
Jack Smith <smith1106@marshall.edu>
2021-04-08 17:05:10 +0000

    Add field search and ordering, pt 2

--- interface/wqbase/views.py
+++ interface/wqbase/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from .models import Stations, Results
 from .serializers import StationsSerializer, ResultsSerializer
 
@@ -11,8 +12,8 @@ class StationsViewSet(viewsets.ModelViewSet):
     queryset = Stations.objects.all()   
     serializer_class = StationsSerializer
     permission_classes = [permissions.IsAuthenticated]
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = [
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = [  # Could have used '__all__'
         'organizationidentifier', 
         'organizationformalname', 
         'monitoringlocationidentifier', 
@@ -50,6 +51,9 @@ class StationsViewSet(viewsets.ModelViewSet):
         'wellholedepthmeasure_measureunitcode', 
         'providername'
     ]
+    search_fields = ['organizationformalname', 'monitoringlocationname']
+    ordering_fields = ['organizationidentifier', 'monitoringlocationidentifier', 'statecode', 'countycode', 'providername']
+    ordering = ['organizationidentifier', 'monitoringlocationidentifier']
 class ResultsViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Results to be viewed or edited.
@@ -57,69 +61,74 @@ class ResultsViewSet(viewsets.ModelViewSet):
     queryset = Results.objects.all()
     serializer_class = ResultsSerializer
     permission_classes = [permissions.IsAuthenticated]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = [
-            'organizationidentifier',
-            'organizationformalname',
-            'activityidentifier',
-            'activitytypecode',
-            'activitymedianame',
-            'activitymediasubdivisionname',
-            'activitystartdate',
-            'activitystarttime_time',
-            'activitystarttime_timezonecode',
-            'activityenddate',
-            'activityendtime_time',
-            'activityendtime_timezonecode',
-            'activitydepthheightmeasure_measurevalue',
-            'activitydepthheightmeasure_measureunitcode',
-            'activitydepthaltitudereferencepointtext',
-            'activitytopdepthheightmeasure_measurevalue',
-            'activitytopdepthheightmeasure_measureunitcode',
-            'activitybottomdepthheightmeasure_measurevalue',
-            'activitybottomdepthheightmeasure_measureunitcode',
-            'projectidentifier',
-            'activityconductingorganizationtext',
-            'monitoringlocationidentifier',
-            'activitycommenttext',
-            'sampleaquifer',
-            'hydrologiccondition',
-            'hydrologicevent',
-            'samplecollectionmethod_methodidentifier',
-            'samplecollectionmethod_methodidentifiercontext',
-            'samplecollectionmethod_methodname',
-            'samplecollectionequipmentname',
-            'resultdetectionconditiontext',
-            'characteristicname',
-            'resultsamplefractiontext',
-            'resultmeasurevalue',
-            'resultmeasure_measureunitcode',
-            'measurequalifiercode',
-            'resultstatusidentifier',
-            'statisticalbasecode',
-            'resultvaluetypename',
-            'resultweightbasistext',
-            'resulttimebasistext',
-            'resulttemperaturebasistext',
-            'resultparticlesizebasistext',
-            'precisionvalue',
-            'resultcommenttext',
-            'usgspcode',
-            'resultdepthheightmeasure_measurevalue',
-            'resultdepthheightmeasure_measureunitcode',
-            'resultdepthaltitudereferencepointtext',
-            'subjecttaxonomicname',
-            'sampletissueanatomyname',
-            'resultanalyticalmethod_methodidentifier',
-            'resultanalyticalmethod_methodidentifiercontext',
-            'resultanalyticalmethod_methodname',
-            'methoddescriptiontext',
-            'laboratoryname',
-            'analysisstartdate',
-            'resultlaboratorycommenttext',
-            'detectionquantitationlimittypename',
-            'detectionquantitationlimitmeasure_measurevalue',
-            'detectionquantitationlimitmeasure_measureunitcode',
-            'preparationstartdate',
-            'providername'
-        ]
+        'organizationidentifier',
+        'organizationformalname',
+        'activityidentifier',
+        'activitytypecode',
+        'activitymedianame',
+        'activitymediasubdivisionname',
+        'activitystartdate',
+        'activitystarttime_time',
+        'activitystarttime_timezonecode',
+        'activityenddate',
+        'activityendtime_time',
+        'activityendtime_timezonecode',
+        'activitydepthheightmeasure_measurevalue',
+        'activitydepthheightmeasure_measureunitcode',
+        'activitydepthaltitudereferencepointtext',
+        'activitytopdepthheightmeasure_measurevalue',
+        'activitytopdepthheightmeasure_measureunitcode',
+        'activitybottomdepthheightmeasure_measurevalue',
+        'activitybottomdepthheightmeasure_measureunitcode',
+        'projectidentifier',
+        'activityconductingorganizationtext',
+        'monitoringlocationidentifier',
+        'activitycommenttext',
+        'sampleaquifer',
+        'hydrologiccondition',
+        'hydrologicevent',
+        'samplecollectionmethod_methodidentifier',
+        'samplecollectionmethod_methodidentifiercontext',
+        'samplecollectionmethod_methodname',
+        'samplecollectionequipmentname',
+        'resultdetectionconditiontext',
+        'characteristicname',
+        'resultsamplefractiontext',
+        'resultmeasurevalue',
+        'resultmeasure_measureunitcode',
+        'measurequalifiercode',
+        'resultstatusidentifier',
+        'statisticalbasecode',
+        'resultvaluetypename',
+        'resultweightbasistext',
+        'resulttimebasistext',
+        'resulttemperaturebasistext',
+        'resultparticlesizebasistext',
+        'precisionvalue',
+        'resultcommenttext',
+        'usgspcode',
+        'resultdepthheightmeasure_measurevalue',
+        'resultdepthheightmeasure_measureunitcode',
+        'resultdepthaltitudereferencepointtext',
+        'subjecttaxonomicname',
+        'sampletissueanatomyname',
+        'resultanalyticalmethod_methodidentifier',
+        'resultanalyticalmethod_methodidentifiercontext',
+        'resultanalyticalmethod_methodname',
+        'methoddescriptiontext',
+        'laboratoryname',
+        'analysisstartdate',
+        'resultlaboratorycommenttext',
+        'detectionquantitationlimittypename',
+        'detectionquantitationlimitmeasure_measurevalue',
+        'detectionquantitationlimitmeasure_measureunitcode',
+        'preparationstartdate',
+        'providername'
+    ]
+    search_fields = ['organizationformalname', 'monitoringlocationidentifier', 'activityenddate', 'activitymedianame', 
+        'projectidentifier', 'characteristicname', 'usgspcode', 'laboratoryname', 'providername']
+    ordering_fields = ['organizationformalname', 'monitoringlocationidentifier', 'activityenddate', 'activitymedianame', 
+        'projectidentifier', 'characteristicname', 'usgspcode', 'laboratoryname', 'providername']
+    ordering = ['monitoringlocationidentifier', 'activityenddate']
